57795a4d33a459176d117c20fc38e9a8a1bbe980
Timothy Chen <timothy.chen01@gmail.com>
2016-12-27 14:15:05 -0500

    Archive old Ruby on Rails project

--- Ruby on Rails (old unfinished version)/app/controllers/pages_controller.rb
+++ Ruby on Rails (old unfinished version)/app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
     #http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    
     render :nothing => true, :status => :unauthorized
   end
 end
--- Ruby on Rails (old unfinished version)/config/application.rb
+++ Ruby on Rails (old unfinished version)/config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 module Umd
   class Application < Rails::Application
