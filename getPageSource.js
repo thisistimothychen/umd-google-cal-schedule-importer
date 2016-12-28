d64cf68b6cfdb63182d8a0ecac0fe93631631d27
bberry1 <brandon.jn.berry@gmail.com>
2016-12-28 15:49:57 -0500

    Add files via upload
    Changed the info that is placed in the popup.html to reflect the classes that are in the grid of the schedule page

--- getPageSource.js
+++ getPageSource.js
@@ -2,29 +2,14 @@
 // Demo: var serialized_html = DOMtoString(document);
 
 function DOMtoString(document_root) {
-    var html = '',
-        node = document_root.firstChild;
-    while (node) {
-        switch (node.nodeType) {
-        case Node.ELEMENT_NODE:
-            html += node.outerHTML;
-            break;
-        case Node.TEXT_NODE:
-            html += node.nodeValue;
-            break;
-        case Node.CDATA_SECTION_NODE:
-            html += '<![CDATA[' + node.nodeValue + ']]>';
-            break;
-        case Node.COMMENT_NODE:
-            html += '<!--' + node.nodeValue + '-->';
-            break;
-        case Node.DOCUMENT_TYPE_NODE:
-            // (X)HTML documents are identified by public identifiers
-            html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-            break;
-        }
-        node = node.nextSibling;
+    var html = '', classes = document_root.getElementsByClassName("calendarGrid-courseBlock-text-bold ng-binding");
+    if (classes.length == 0){
+        html = "NO CLASSES FOUND";
     }
+    for(i = 0 ; i < classes.length; i++){
+        html += classes[i].innerHTML + "\n";
+    }
+
     return html;
 }
 
@@ -34,3 +19,5 @@ chrome.runtime.sendMessage({
 });
 
 console.log("HERE");
+
+
