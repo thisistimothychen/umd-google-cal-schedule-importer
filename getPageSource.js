928f6e481a35659bd430736e8958aa65f38d9639
Timothy Chen <timothy.chen01@gmail.com>
2016-12-28 18:50:38 -0500

    Add minor changes
    Remove leading space on course name
    
    Change order of popup.html elements

--- getPageSource.js
+++ getPageSource.js
@@ -6,12 +6,16 @@ function DOMtoString(document_root) {
     var html = '', 
         ccontainers = document_root.getElementsByClassName("course-card-container--info"),
         temp = null;
-    if (ccontainers.length == 0){
-        html = "NO CLASSES FOUND";
+    if (ccontainers.length == 0) {
+        html = "Please navigate to the Testudo Show Schedule page";
     }
    
-    for(i = 0 ; i < ccontainers.length; i++){
+    for(i = 0 ; i < ccontainers.length; i++) {
+      if (i == 0) {
+        html += ccontainers[i].innerText.substring(1) + "END";
+      } else {
         html += ccontainers[i].innerText + "END";
+      }
     }
  
 
