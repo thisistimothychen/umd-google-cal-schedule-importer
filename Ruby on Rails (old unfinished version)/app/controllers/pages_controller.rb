08d85cc0d11d8da035174f598617df5a3a622d6d
Timothy Chen <timothy.chen01@gmail.com>
2016-12-29 01:49:19 -0500

    Clean up repository
    Remove old unfinished Ruby on Rails version

--- Ruby on Rails (old unfinished version)/app/controllers/pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesController < ApplicationController
-  def index
-    #http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    
-    render :nothing => true, :status => :unauthorized
-  end
-end
