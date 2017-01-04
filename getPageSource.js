5de4ee16337f1cd49b9edef33789d9e394c22c4d
Timothy Chen <timothy.chen01@gmail.com>
2017-01-04 17:39:01 -0500

    Parse schedule into JSON format

--- getPageSource.js
+++ getPageSource.js
@@ -1,8 +1,19 @@
 // @author Rob W <http://stackoverflow.com/users/938089/rob-w>
 // Demo: var serialized_html = DOMtoString(document);
 
+function getSemesterFirstDay() {
+  // TODO dynamically get this using web scraping from https://www.provost.umd.edu/calendar/16.cfm
+  return new Date ("January 25, 2017");
+}
+
+function getSemesterLastDay() {
+  // TODO dynamically get this using web scraping from https://www.provost.umd.edu/calendar/16.cfm
+  return new Date ("May 11, 2017");
+}
 
 function DOMtoString(document_root) {
+
+
     var html = '',
         ccontainers = document_root.getElementsByClassName("course-card-container--info"),
         courseEventInfo = new Array();
@@ -83,42 +94,124 @@ function DOMtoString(document_root) {
       TBA
       */
 
-      // TODO parse each course into json or array format here
+      // Parse each course
       courseLines = courseInfo.split("\n")
-
-      timeLines = new Array();
-      roomLocations = new Array();
+      var classTypes = new Array();
+      var timeLines = new Array();
+      var roomLocations = new Array();
       for (j = 0; j < courseLines.length; j++) {
-        line = courseLines[j];
+        var line = courseLines[j];
 
         if (j == 0) {  // Course Title
-          firstLine = line.split(" ");
+          var firstLine = line.split(" ");
           courseTitle = firstLine[0] + " " + firstLine[1];
           sectionCode = firstLine[2].substring(1,firstLine[2].length-1);
         } else if (line == "Lec" || line == "Dis") {
-          if (courseLines[j+1] == "TBA") {  /* time is TBA */
+          if (courseLines[j+1] == "TBA") {  /* if time is TBA */
             // do nothing.
-          } else {
+          } else {  // other info
+            classTypes.push(courseLines[j]);
             timeLines.push(courseLines[j+1]);
             roomLocations.push(courseLines[j+2]);
           }
-        } else if (line == "Final") {
+        } else if (line == "Final") { // end here; we don't want the final because generally not available yet
           break;
         }
       }
 
-      // courseEventInfo.push({
-      //
-      // });
+      // reformat data into usable variables
+      for (j = 0; j < timeLines.length; j++) {
+        // set starting variables
+        classType = classTypes[j];
+        timeLine = timeLines[j];
+        roomLocation = roomLocations[j];
+
+        // parse class time
+        // M/T/W/Th/F (each optional), ##:##(a/p)m - ##:##(a/p)m
+        // W 11:00am - 12:15pm
+        timeLineInfo = timeLine.split(" ");
+        daysStr = timeLineInfo[0];
+        startTime = timeLineInfo[1];
+        endTime = timeLineInfo[3];
+
+        // get semester start/end dates
+        semFirstDate = getSemesterFirstDay();
+        semEndDate = getSemesterLastDay();
+        semFirstDay = semFirstDate.getDay();
+        //semEndDay = // TODO Need or don't need? Look at GCal API repeat requirements
+
+        // separate out timecodes and parse into integers
+        startHour = parseInt(startTime.match(/(\d+)/g)[0]);
+        startMin = parseInt(startTime.match(/(\d+)/g)[1]);
+        endHour = parseInt(endTime.match(/(\d+)/g)[0]);
+        endMin = parseInt(endTime.match(/(\d+)/g)[1]);
+
+        // regex to separate each day of the week
+        daysArray = daysStr.match(/([A-Z][a-z]*)/g)
+        classStartDay = 0;  // instantiate the day for comparison with semFirstDay
+        for (k = 0; k < daysArray.length; k++) {
+          switch(daysArray[k]) {
+            case "M":
+              classStartDay = 1;
+              break;
+            case "T":
+              classStartDay = 2;
+              break;
+            case "W":
+              classStartDay = 3;
+              break;
+            case "Th":
+              classStartDay = 4;
+              break;
+            case "F":
+              classStartDay = 5;
+              break;
+          }
+
+          dayOffset = semFirstDay - classStartDay;
+
+          // create duplicates for class start/end datetimes
+          var classStartDate = new Date(semFirstDate.getTime());
+          classStartDate.setHours(startHour);
+          classStartDate.setMinutes(startMin);
+          var classEndDate = new Date(semFirstDate.getTime());
+          classEndDate.setHours(endHour);
+          classEndDate.setMinutes(endMin);
 
 
-      console.log("Course name: " + courseTitle);
-      console.log("Section: " + sectionCode);
-      console.log("Location: " + roomLocations.toString());
-      console.log("Time: " + timeLines.toString());
+          if (dayOffset == 0) {       // class day is same as semester start day
+            // do nothing; the day is correct
+          } else if (dayOffset > 0) { // class day is before semester start day (need to go to next week)
+            classStartDate.setDate(classStartDate.getDate()+7-dayOffset);
+            classEndDate.setDate(classEndDate.getDate()+7-dayOffset);
+          } else {                    // class day is after semester start day (simply add onto the semFirstDay)
+            classStartDate.setDate(classStartDate.getDate()+Math.abs(dayOffset));
+            classEndDate.setDate(classEndDate.getDate()+Math.abs(dayOffset));
+          }
+
+          // store individual event's data in json format
+          courseEventInfo.push({
+            "courseTitle": courseTitle,
+            "section": sectionCode,
+            "location": roomLocation,
+            "startDate": classStartDate,
+            "endDate": classEndDate
+          });
+
+          console.log("Start Date: " + classStartDate);
+          console.log("End Date: " + classEndDate);
+          console.log("Start Time: " + startHour + ":" + startMin);
+          console.log("End Time: " + endHour + ":" + endMin);
+          // console.log("Course name: " + courseTitle);
+          // console.log("Section: " + sectionCode);
+          // console.log("Class Type: " + classTypes.toString());
+          // console.log("Location: " + roomLocations.toString());
+          // console.log("Time: " + timeLines.toString());
+        }
+      }
     }
 
-    // console.log(html);
+    console.log(courseEventInfo);
 
     // TODO also return the json or array holding courses
     return [html, validPage];
